f61b5fa05936698a281be3940d9cfb191e9e3016
David Khourshid <davidkpiano@gmail.com>
2023-10-11 19:15:55 -0400

    This'll do

--- src/client.ts
+++ src/client.ts
@@ -1,40 +1,238 @@
-import "./styles.css";
+import './styles.css';
 
-import PartySocket from "partysocket";
+import PartySocket from 'partysocket';
+import { html, render } from 'lit-html';
+import type { Leaderboard } from './server';
+
+import {
+  assign,
+  createActor,
+  createMachine,
+  fromPromise,
+  type AnyState,
+  fromCallback,
+} from 'xstate';
+
+const view = (state: AnyState, leaderboard: Leaderboard) => {
+  return html`
+    <header>
+      ${state.hasTag('noPermission')
+        ? html`<h1>y u no permission</h1>`
+        : html`<h1>Weatherboard</h1>`}
+    </header>
+    <div class="items">
+      ${state.context.name && !leaderboard[state.context.id]
+        ? html`
+            <div
+              class="item -loading"
+              style="view-transition-name: ${state.context.id}"
+            >
+              <div class="user-weather">
+                <div></div>
+                <div>??&deg;</div>
+              </div>
+              <div class="user-name">${state.context.name}</div>
+              <div class="user-location">Not sure yet...</div>
+            </div>
+          `
+        : ''}
+      ${Object.entries(leaderboard)
+        .sort(
+          ([, itemA], [, itemB]) => itemB.current.temp_c - itemA.current.temp_c
+        )
+        .map(
+          ([id, { name, location, current }], i) => html`
+            <div
+              class="item"
+              style="--i: ${i}; view-transition-name: ${id};"
+              data-index="${i}"
+            >
+              <div class="user-weather">
+                <img src="${current.condition.icon}" />
+                <div>${current.temp_c}&deg;</div>
+              </div>
+              <div class="user-name">${name}</div>
+              <div class="user-location">
+                ${location.name}, ${location.country}
+              </div>
+            </div>
+          `
+        )}
+    </div>
+  `;
+};
 
 declare const PARTYKIT_HOST: string;
 
-let pingInterval: ReturnType<typeof setInterval>;
+const elApp = document.getElementById('app') as HTMLDivElement;
 
-// Let's append all the messages we get into this DOM element
-const output = document.getElementById("app") as HTMLDivElement;
-
-// Helper function to add a new line to the DOM
-function add(text: string) {
-  output.appendChild(document.createTextNode(text));
-  output.appendChild(document.createElement("br"));
-}
-
-// A PartySocket is like a WebSocket, except it's a bit more magical.
-// It handles reconnection logic, buffering messages while it's offline, and more.
-const conn = new PartySocket({
+const partySocket = new PartySocket({
   host: PARTYKIT_HOST,
-  room: "my-new-room",
+  room: 'my-new-room',
 });
 
-// You can even start sending messages before the connection is open!
-conn.addEventListener("message", (event) => {
-  add(`Received -> ${event.data}`);
+const machine = createMachine({
+  initial: 'connecting',
+  types: {
+    context: {} as {
+      name: string | null;
+      location: string | null;
+      leaderboard: Leaderboard;
+      id: string;
+    },
+    input: {} as {
+      id: string;
+    },
+  },
+  context: ({ input }) => {
+    return {
+      name: null,
+      location: null,
+      leaderboard: {},
+      id: input.id,
+    };
+  },
+  invoke: {
+    src: fromCallback(({ sendBack }) => {
+      partySocket.addEventListener('message', (event) => {
+        const eventObject = JSON.parse(event.data);
+        sendBack(eventObject);
+      });
+    }),
+  },
+  states: {
+    connecting: {
+      invoke: {
+        src: fromPromise(() => {
+          return new Promise((res) => {
+            partySocket.addEventListener('open', () => {
+              res(partySocket.id);
+            });
+          });
+        }),
+        onDone: {
+          target: 'connected',
+          actions: assign({}),
+        },
+      },
+    },
+    connected: {
+      initial: 'getName',
+      states: {
+        getName: {
+          tags: 'getName',
+          invoke: {
+            src: fromPromise(async () => {
+              const name = prompt('What is your name?');
+              return Promise.resolve(name);
+            }),
+            onDone: {
+              actions: assign({
+                name: ({ event }) => event.output,
+              }),
+              // target: 'getLocation',
+            },
+          },
+          always: {
+            guard: ({ context }) => !!context.name,
+            target: 'getLocation',
+          },
+        },
+        getLocation: {
+          invoke: {
+            src: fromPromise(async () => {
+              if (!navigator.geolocation) {
+                return Promise.reject('no permission');
+              }
+
+              // get location from browser
+              const location = await new Promise((res, rej) =>
+                navigator.geolocation.getCurrentPosition(
+                  (pos) => {
+                    res(`${pos.coords.latitude},${pos.coords.longitude}`);
+                  },
+                  (err) => {
+                    rej(err);
+                  }
+                )
+              );
+
+              const res = Promise.resolve(location);
+
+              return res;
+            }),
+            onError: {
+              target: 'noPermission',
+              actions: () => {
+                console.log('huh?');
+              },
+            },
+            onDone: [
+              {
+                guard: ({ event }) => event.output,
+                actions: [
+                  assign({
+                    location: ({ event }) => event.output,
+                  }),
+                  ({ context }) => {
+                    partySocket.send(
+                      JSON.stringify({
+                        type: 'register',
+                        name: context.name,
+                        location: context.location,
+                      })
+                    );
+                  },
+                ],
+                target: 'leaderboard',
+              },
+              { target: 'noPermission' },
+            ],
+          },
+          always: {
+            guard: ({ context }) => context.location !== null,
+            target: 'leaderboard',
+          },
+        },
+        noPermission: {
+          tags: 'noPermission',
+        },
+        leaderboard: {
+          entry: () => {
+            partySocket.send(JSON.stringify({ type: 'sup' }));
+          },
+        },
+      },
+    },
+  },
+  on: {
+    'leaderboard.updated': {
+      actions: assign({
+        leaderboard: ({ event }) => event.leaderboard,
+      }),
+    },
+  },
 });
 
-// Let's listen for when the connection opens
-// And send a ping every 2 seconds right after
-conn.addEventListener("open", () => {
-  add("Connected!");
-  add("Sending a ping every 2 seconds...");
-  // TODO: make this more interesting / nice
-  clearInterval(pingInterval);
-  pingInterval = setInterval(() => {
-    conn.send("ping");
-  }, 1000);
+const actor = createActor(machine, {
+  input: {
+    id: partySocket.id,
+  },
+  inspect: (ev) => {
+    if (ev.type === '@xstate.event' && ev.targetRef === actor) {
+      console.log(ev);
+    }
+  },
 });
+
+actor.subscribe((s) => {
+  if ('startViewTransition' in document) {
+    (document as any).startViewTransition(() => {
+      render(view(actor.getSnapshot(), s.context.leaderboard), elApp);
+    });
+  } else {
+    render(view(actor.getSnapshot(), s.context.leaderboard), elApp);
+  }
+});
+
+actor.start();
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,87 @@
-import type * as Party from "partykit/server";
+import type * as Party from 'partykit/server';
+
+const WEATHER_API_KEY = '36dd02d5282f473caec170818230810';
+
+const createUrl = (location: string) =>
+  `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location}&aqi=no`;
+
+export type Leaderboard = Record<
+  string,
+  {
+    name: string;
+    location: Location;
+    current: Current;
+  }
+>;
+
+interface Location {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  tz_id: string;
+  localtime_epoch: number;
+  localtime: string;
+}
+
+interface Condition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+interface Current {
+  last_updated_epoch: number;
+  last_updated: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: Condition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
+}
+
+interface WeatherResponse {
+  location: Location;
+  current: Current;
+}
+
+const leaderboard: Leaderboard = {};
+
+async function refetch(leaderboard: Leaderboard) {
+  const promises = Object.values(leaderboard).map(({ location }) =>
+    fetch(createUrl(location.name))
+  );
+  const responses = await Promise.all(promises);
+  const jsons = await Promise.all(responses.map((r) => r.json()));
+  const newLeaderboard = Object.fromEntries(
+    Object.entries(leaderboard).map(([id, user], i) => [
+      id,
+      {
+        ...user,
+        current: jsons[i].current,
+      },
+    ])
+  );
+
+  return newLeaderboard;
+}
 
 export default class Server implements Party.Server {
   constructor(readonly party: Party.Party) {}
@@ -12,19 +95,64 @@ export default class Server implements Party.Server {
   url: ${new URL(ctx.request.url).pathname}`
     );
 
-    // let's send a message to the connection
-    conn.send("hello from server");
+    conn.send(
+      JSON.stringify({
+        type: 'leaderboard.updated',
+        leaderboard: leaderboard,
+      })
+    );
+  }
+
+  onStart(): void | Promise<void> {
+    setInterval(async () => {
+      const newLeaderboard = await refetch(leaderboard);
+
+      Object.assign(leaderboard, newLeaderboard);
+
+      this.party.broadcast(
+        JSON.stringify({
+          type: 'leaderboard.updated',
+          leaderboard,
+        })
+      );
+    }, 1000 * 60 * 10);
   }
 
   onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.party.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
+    const eventObject = JSON.parse(message);
+
+    console.log(
+      `connection ${sender.id} sent message with type: ${eventObject.type}`
     );
+
+    if (eventObject.type === 'register') {
+      this.party.broadcast(
+        JSON.stringify({
+          type: 'leaderboard.registered',
+          name: eventObject.name,
+          senderId: sender.id,
+        })
+      );
+      fetch(createUrl(eventObject.location))
+        .then((response) => response.json())
+        .then((data: WeatherResponse) => {
+          leaderboard[sender.id] = {
+            name: eventObject.name,
+            location: data.location,
+            current: data.current,
+          };
+
+          this.party.broadcast(
+            JSON.stringify({
+              type: 'leaderboard.updated',
+              leaderboard: leaderboard,
+            })
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   }
 }
 
